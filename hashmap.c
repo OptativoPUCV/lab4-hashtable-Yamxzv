7d19745fbe2b864103416f4e462b4dd07f6fb8ff
Yamxzv <163350591+Yamxzv@users.noreply.github.com>
2024-04-27 17:28:46 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -97,8 +97,17 @@ HashMap * createMap(long capacity)
 
 void eraseMap(HashMap * map,  char * key) 
 {    
-
-
+  int position = hash(key, map->capacity);
+  while (map->buckets[position] != NULL)
+  {
+    if (is_equal(map->buckets[position]->key, key))
+    {
+      map->buckets[position] = NULL;
+      map->size--;
+      return;
+    }
+    position = (position + 1) % map->capacity;
+  }
 }
 
 Pair * searchMap(HashMap * map,  char * key) 
@@ -107,18 +116,19 @@ Pair * searchMap(HashMap * map,  char * key)
 
   int position = hash(key, map->capacity);
   while (map->buckets[position] != NULL)
+  {
+    if (is_equal(map->buckets[position]->key, key))
     {
-      if (is_equal(map->buckets[position]->key, key))
-      {
-        map->current = position;
-        return map->buckets[position];
-      }
-      position = (position + 1) % map->capacity;
+      map->current = position;
+      return map->buckets[position];
     }
-    return NULL;
+    position = (position + 1) % map->capacity;
+  }
+  return NULL;
 }
 
-Pair * firstMap(HashMap * map) {
+Pair * firstMap(HashMap * map) 
+{
 
     return NULL;
 }
