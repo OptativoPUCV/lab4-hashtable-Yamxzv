68a48f682b281525e3a6cc77f6220d0c9cc2bdf7
Yamxzv <163350591+Yamxzv@users.noreply.github.com>
2024-04-27 17:31:45 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -129,8 +129,17 @@ Pair * searchMap(HashMap * map,  char * key)
 
 Pair * firstMap(HashMap * map) 
 {
+  if (map == NULL) return NULL;
 
-    return NULL;
+  for (int i = 0; i < map->capacity;i++)
+  {
+    if (map->buckets[i] != NULL)
+    {
+      map->current = i;
+      return map->buckets[i];
+    }
+  }
+  return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
